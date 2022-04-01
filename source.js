613b93f81f79efd13237dc9fc4eb2d2c1e94d452
Doomsdaybear <101878029+Doomsdaybear@users.noreply.github.com>
2022-04-01 16:56:18 -0400

    Create source.js

---
+++ source.js
@@ -0,0 +1,52 @@
+//The most primitive AI ever.
+var input = prompt("Rock,paper, or scissors? (r/p/s)")
+function Input() {
+  prompt("Rock,paper,or scissors? (r/p/s)")
+}
+if(input == "r"){
+ var choosing = Math.floor(Math.random() * 4)
+ if(choosing == 0){
+   alert("Whoops!Bot chose 0, nobody wins!");
+   
+ } 
+ if(choosing == 1){
+    
+    alert("Rock, its a tie");
+    
+  }
+  if(choosing == 2){
+    console.log("Bot chose paper")
+    alert("Paper,bot wins!");
+    
+  }
+  if(choosing == 3){
+    alert("Scissors, you win!");
+    
+  }
+}
+if(input == "p"){
+  var choosing = Math.floor(Math.random() * 4)
+  if(choosing == 1){
+    alert("Rock, you win!");
+    
+  }
+  if(choosing == 2){
+    alert("Paper, its a tie");
+    
+  }
+  if(choosing == 3){
+    alert("Scissors, bot wins!")
+  }
+}
+if(input == "s"){
+   var choosing = Math.floor(Math.random() * 4)
+   if(choosing == 1){
+     alert("Rock,bot wins!")
+   }
+  if(choosing == 2){
+    alert("Paper,you win!")
+  }
+  if(choosing == 3){
+    alert("Scissors, its a tie")
+  }
+   }
